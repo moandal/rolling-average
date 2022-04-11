de0e6939c69cbb0945cf4a5efb23300c23aa90ac
Allan Phillips <allan@allanphillips.co.uk>
2022-04-11 20:27:49 +0100

    Stable version

--- app/src/main/java/com/moandal/rollingaverage/RAData.java
+++ app/src/main/java/com/moandal/rollingaverage/RAData.java
@@ -13,46 +13,30 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class RAData extends AppCompatActivity{
+public class RAData extends AppCompatActivity {
+/*
+    double rollingAverage;
+    int rollingNumber; // number of readings to average over
+    int decimalPlaces; // number of decimal places for rounding of rolling average
+    int numberToDisplay; // number of readings in history to display
+    double[] readings;
+    double[] rollingAvs;
+ //   Date[] readDates = new Date[R.integer.array_size];
+ //   SimpleDateFormat ddmmFormat = new SimpleDateFormat("dd/MM/yyyy");
 
-    public double rollingAverage;
-    public int rollingNumber; // number of readings to average over
-    public int decimalPlaces; // number of decimal places for rounding of rolling average
-    public int numberToDisplay; // number of readings in history to display
-    public int arraySize = 100;
-    public double[] readings = new double[arraySize];
-    public double[] rollingAvs = new double[arraySize];
-    public Date[] readDates = new Date[arraySize];
-    SimpleDateFormat ddmmFormat = new SimpleDateFormat("dd/MM/yyyy");
-
-    public void loadData(Activity activity) {
-
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(activity);
-        rollingNumber = Integer.parseInt(preferences.getString("rolling_number", "7"));
-        decimalPlaces = Integer.parseInt(preferences.getString("decimal_places", "2"));
-        numberToDisplay = Integer.parseInt(preferences.getString("number_to_display", "7"));
-
-        SharedPreferences sp = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-        for (int i = 0; i < arraySize; i++) {
-            readings[i] = Double.valueOf(sp.getString("Weight" + i, "0"));
-            readDates[i] = Utils.convertStringToDate(sp.getString("readDates" + i, "0"));
-        }
+    RAData(double rollingAverage, int rollingNumber, int decimalPlaces, int numberToDisplay, double[] readings, double[] rollingAvs) {
+        this.rollingAverage = rollingAverage;
+        this.rollingNumber = rollingNumber;
+        this.decimalPlaces = decimalPlaces;
+        this.numberToDisplay = numberToDisplay;
+        this.readings = readings;
+        this.rollingAvs = rollingAvs;
     }
 
-    public void saveData() {
-        SharedPreferences sp = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        for (int i = 0; i < arraySize; i++) {
-            editor.putString("Weight" + i, Double.toString(readings[i]));
-            editor.putString("readDates" + i, ddmmFormat.format(readDates[i]));
-        }
-        editor.commit();
-    }
-
-    public void calcAvs() {
+    void calcAvs() {
 
         double multiplier = Math.pow(10, decimalPlaces);
-        int startIndex = arraySize - rollingNumber;
+        int startIndex = R.integer.array_size - rollingNumber;
 
         for (int i = startIndex; i >= 0; i--) {
 
@@ -69,5 +53,30 @@ public class RAData extends AppCompatActivity{
         }
 
     }
+/*
+    public void loadData(Activity activity) {
 
+        Context context = activity;
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        rollingNumber = Integer.parseInt(preferences.getString("rolling_number", "7"));
+        decimalPlaces = Integer.parseInt(preferences.getString("decimal_places", "2"));
+        numberToDisplay = Integer.parseInt(preferences.getString("number_to_display", "7"));
+
+        SharedPreferences sp = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        for (int i = 0; i < R.integer.array_size; i++) {
+            readings[i] = Double.valueOf(sp.getString("Weight" + i, "0"));
+            readDates[i] = Utils.convertStringToDate(sp.getString("readDates" + i, "0"));
+        }
+    }
+
+    public void saveData() {
+        SharedPreferences sp = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        for (int i = 0; i < R.integer.array_size; i++) {
+            editor.putString("Weight" + i, Double.toString(readings[i]));
+            editor.putString("readDates" + i, ddmmFormat.format(readDates[i]));
+        }
+        editor.commit();
+    }
+*/
 }
